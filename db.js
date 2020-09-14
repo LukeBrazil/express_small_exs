b8e7cf9256b1e89630aba1ae6fad88531c16da28
Luke Brazil <Luke.brazil13@icloud.com>
2020-09-14 15:10:14 -0500

    added map to access db namd and species

---
+++ db.js
@@ -0,0 +1,24 @@
+const monsters = [
+    {
+        name: "Dracula",
+        species: "Vampire",
+        slug: "dracula"
+    },
+    {
+        name: "Frankensteins Monster",
+        species: "Zombie",
+        slug: "frankenstein"
+    },
+    {
+        name: "Visible Man",
+        species: "Human",
+        slug: "invisible_man"
+    },
+    {
+        name: "Wolf Man",
+        species: "Lycan",
+        slug: "wolfman"
+    }
+]
+
+module.exports = monsters;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const hostname = '127.0.0.1';
 const port = 3333;
+const db = require('./db');
 
 const app = express();
 
@@ -56,9 +57,54 @@ const greetController = (req, res) => {
         .end();
 }
 
+const monsterController = (req, res) => {
+    let snippet = `<h1>BOOO!!!</h1>`;
+
+    if (req.params.name === undefined && db !== undefined) {
+        snippet += `<ul>`
+    db.map(monster => {
+        snippet += `<li><a href="/monster/${monster.slug}">${monster.name}</a></li>`;
+    });
+    snippet += `</ul>`
+    }
+
+    if (req.params.name !== undefined) {
+         snippet = `<h1>BOOO!!!</h1>`;
+        db.map(monster => {
+            if (req.params.name === monster.slug) {
+                snippet += `<p> I am ${monster.name} and I am a ${monster.species}. </p>`
+            }
+        });
+    }
+    res
+        .status(200)
+        .send(snippet)
+        .end();
+}
+
 
 app.get('/', rootController);
 app.get('/cats', catController);
 app.get('/dogs', dogsController);
 app.get('/cats_and_dogs', catanddogsController);
 app.get('/greet/:name?', greetController);
+app.get('/monster/:name?', monsterController);
+
+// Erics switch solution
+
+// if (req.params.name !== undefined) {
+    //     switch (req.params.name) {
+    //         case db[0].slug:
+    //             snippet += `<h1>It is ${db[0].name} and I am a ${db[0].species}!</h1>`
+    //             break;
+    //         case db[1].slug:
+    //             snippet += `<h1>It is ${db[1].name} and I am a ${db[1].species}!</h1>`
+    //             break;
+    //         case db[2].slug:
+    //             snippet += `<h1>It is ${db[2].name} and I am a ${db[2].species}!</h1>`
+    //             break;
+    //         case db[3].slug:
+    //             snippet += `<h1>It is ${db[3].name} and I am a ${db[3].species}!</h1>`
+    //             break;
+    //     }
+    // }
